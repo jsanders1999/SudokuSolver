231a84048cdc4ff0f0009a4dc268dd61c1978006
Julian Sanders <j.a.sanders@student.tudelft.nl>
2024-06-21 23:50:56 +0200

    replaced quadruple spaces with tabs

--- codes/python/utils/constraintUtils.py
+++ codes/python/utils/constraintUtils.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy as sp
 from numba import njit
 import numba
-
+	
 
 #these are operatiors that should apply to a 729 long solution vector x
 #x 
@@ -11,162 +11,162 @@ NROWS = 9
 NVALS = 9
 
 def generate_one_value_constr_sparse(format = "csr", dtype = int):
-    #[1,1,1,1,1,1,1,1,1, 0,0,0,0,0,....]
-    #[0,0,0,0,0,0,0,0,0, 1,1,1,1,1,....
-    #...
-    return sp.sparse.kron(
-        sp.sparse.eye(NROWS*NCOLS, dtype = dtype, format=format),
-        np.ones((1,NVALS), dtype = dtype),
-        format=format
-    )
+	#[1,1,1,1,1,1,1,1,1, 0,0,0,0,0,....]
+	#[0,0,0,0,0,0,0,0,0, 1,1,1,1,1,....
+	#...
+	return sp.sparse.kron(
+		sp.sparse.eye(NROWS*NCOLS, dtype = dtype, format=format),
+		np.ones((1,NVALS), dtype = dtype),
+		format=format
+	)
 
 def generate_one_value_constr_dense(dtype = int):
-    #[1,1,1,1,1,1,1,1,1, 0,0,0,0,0,....]
-    #[0,0,0,0,0,0,0,0,0, 1,1,1,1,1,....
-    #...
-    return np.kron(
-        np.eye(NROWS*NCOLS, dtype = dtype),
-        np.ones((1,NVALS), dtype = dtype)
-    )
+	#[1,1,1,1,1,1,1,1,1, 0,0,0,0,0,....]
+	#[0,0,0,0,0,0,0,0,0, 1,1,1,1,1,....
+	#...
+	return np.kron(
+		np.eye(NROWS*NCOLS, dtype = dtype),
+		np.ones((1,NVALS), dtype = dtype)
+	)
 
 @njit
 def generate_one_value_constr_njit():
-    #[1,1,1,1,1,1,1,1,1, 0,0,0,0,0,....]
-    #[0,0,0,0,0,0,0,0,0, 1,1,1,1,1,....
-    #...
-    return np.kron(
-        np.eye(NROWS*NCOLS, dtype=numba.boolean),
-        np.ones((1,NVALS), dtype=numba.boolean)
-    )
+	#[1,1,1,1,1,1,1,1,1, 0,0,0,0,0,....]
+	#[0,0,0,0,0,0,0,0,0, 1,1,1,1,1,....
+	#...
+	return np.kron(
+		np.eye(NROWS*NCOLS, dtype=numba.boolean),
+		np.ones((1,NVALS), dtype=numba.boolean)
+	)
 
 
 def generate_row_uniq_constr_sparse(format = "csr", dtype = int):
-    #[1,..,0, 1,..,0, ... 1,... 0,  0,1,..,0, 0, 
-    return sp.sparse.kron(
-        sp.sparse.eye(NROWS, dtype = dtype, format=format),
-        sp.sparse.kron(np.ones((1,NCOLS), dtype = dtype), 
-                       sp.sparse.eye(NVALS, dtype = dtype),
-                       format=format),
-        format=format
-    )
+	#[1,..,0, 1,..,0, ... 1,... 0,  0,1,..,0, 0, 
+	return sp.sparse.kron(
+		sp.sparse.eye(NROWS, dtype = dtype, format=format),
+		sp.sparse.kron(np.ones((1,NCOLS), dtype = dtype), 
+					   sp.sparse.eye(NVALS, dtype = dtype),
+					   format=format),
+		format=format
+	)
 
 def generate_row_uniq_constr_dense(dtype = int):
-    #[1,..,0, 1,..,0, ... 1,... 0,  0,1,..,0, 0, 
-    return np.kron(
-        np.eye(NROWS, dtype = dtype),
-        np.kron(np.ones((1,NCOLS), dtype = dtype), 
-                       np.eye(NVALS, dtype =dtype))
-    )
+	#[1,..,0, 1,..,0, ... 1,... 0,  0,1,..,0, 0, 
+	return np.kron(
+		np.eye(NROWS, dtype = dtype),
+		np.kron(np.ones((1,NCOLS), dtype = dtype), 
+					   np.eye(NVALS, dtype =dtype))
+	)
 
 @njit
 def generate_row_uniq_constr_njit():
-    #[1,..,0, 1,..,0, ... 1,... 0,  0,1,..,0, 0, 
-    return np.kron(
-        np.eye(NROWS, dtype = numba.boolean),
-        np.kron(np.ones((1,NCOLS), dtype = numba.boolean), 
-                       np.eye(NVALS, dtype = numba.boolean))
-    )
+	#[1,..,0, 1,..,0, ... 1,... 0,  0,1,..,0, 0, 
+	return np.kron(
+		np.eye(NROWS, dtype = numba.boolean),
+		np.kron(np.ones((1,NCOLS), dtype = numba.boolean), 
+					   np.eye(NVALS, dtype = numba.boolean))
+	)
 
 
 def generate_col_uniq_constr_sparse(format = "csr", dtype = int):
-    return sp.sparse.kron(
-        np.ones((1,NROWS), dtype = dtype),
-        sp.sparse.eye(NCOLS*NVALS, dtype = dtype, format=format),
-        format=format
-    )
+	return sp.sparse.kron(
+		np.ones((1,NROWS), dtype = dtype),
+		sp.sparse.eye(NCOLS*NVALS, dtype = dtype, format=format),
+		format=format
+	)
 
 def generate_col_uniq_constr_dense(dtype = int):
-    return np.kron(
-        np.ones((1,NROWS), dtype = dtype),
-        np.eye(NCOLS*NVALS, dtype = dtype)
-    )
+	return np.kron(
+		np.ones((1,NROWS), dtype = dtype),
+		np.eye(NCOLS*NVALS, dtype = dtype)
+	)
 
 @njit
 def generate_col_uniq_constr_njit():
-    return np.kron(
-        np.ones((1,NROWS), dtype = numba.boolean),
-        np.eye(NCOLS*NVALS, dtype = numba.boolean)
-    )
-    
+	return np.kron(
+		np.ones((1,NROWS), dtype = numba.boolean),
+		np.eye(NCOLS*NVALS, dtype = numba.boolean)
+	)
+	
 
 def generate_block_uniq_constr_sparse(format = "csr", dtype = int):
-    return sp.sparse.kron(
-        sp.sparse.eye(3, dtype = dtype, format=format),
-        sp.sparse.kron(
-            np.ones((1,3), dtype = dtype), 
-            sp.sparse.kron(
-                sp.sparse.eye(3, dtype = dtype, format=format),
-                sp.sparse.kron( 
-                    np.ones((1,3), dtype = dtype),
-                    sp.sparse.eye(9, dtype = dtype, format=format)
-                ),
-                format=format
-            ),
-            format=format   
-        ),
-        format=format
-    )
+	return sp.sparse.kron(
+		sp.sparse.eye(3, dtype = dtype, format=format),
+		sp.sparse.kron(
+			np.ones((1,3), dtype = dtype), 
+			sp.sparse.kron(
+				sp.sparse.eye(3, dtype = dtype, format=format),
+				sp.sparse.kron( 
+					np.ones((1,3), dtype = dtype),
+					sp.sparse.eye(9, dtype = dtype, format=format)
+				),
+				format=format
+			),
+			format=format   
+		),
+		format=format
+	)
 
 def generate_block_uniq_constr_dense(dtype = int):
-    return np.kron(
-        np.eye(3, dtype = dtype),
-        np.kron(
-            np.ones((1,3), dtype = dtype), 
-            np.kron(
-                np.eye(3, dtype = dtype),
-                np.kron( 
-                    np.ones((1,3), dtype = dtype),
-                    np.eye(9, dtype = dtype)
-                ),
-            ),
-        ),
-    )
+	return np.kron(
+		np.eye(3, dtype = dtype),
+		np.kron(
+			np.ones((1,3), dtype = dtype), 
+			np.kron(
+				np.eye(3, dtype = dtype),
+				np.kron( 
+					np.ones((1,3), dtype = dtype),
+					np.eye(9, dtype = dtype)
+				),
+			),
+		),
+	)
 
 @njit
 def generate_block_uniq_constr_njit():
-    return np.kron(
-        np.eye(3, dtype = numba.boolean),
-        np.kron(
-            np.ones((1,3), dtype = numba.boolean), 
-            np.kron(
-                np.eye(3, dtype = numba.boolean),
-                np.kron( 
-                    np.ones((1,3), dtype = numba.boolean),
-                    np.eye(9, dtype = numba.boolean)
-                ),
-            ),
-        ),
-    )
+	return np.kron(
+		np.eye(3, dtype = numba.boolean),
+		np.kron(
+			np.ones((1,3), dtype = numba.boolean), 
+			np.kron(
+				np.eye(3, dtype = numba.boolean),
+				np.kron( 
+					np.ones((1,3), dtype = numba.boolean),
+					np.eye(9, dtype = numba.boolean)
+				),
+			),
+		),
+	)
 
 def generate_clues_contr_sparse(clues_inds_rows, clues_inds_cols, clues_vals,
-                         format = "csr", dtype = int):
-    #x[i,j,m] = 1
-    op_shape = (clues_vals.shape[0], NROWS*NCOLS*NVALS)
+						 format = "csr", dtype = int):
+	#x[i,j,m] = 1
+	op_shape = (clues_vals.shape[0], NROWS*NCOLS*NVALS)
 
-    data = np.ones((clues_vals.shape[0]), dtype=dtype)
-    row_inds = np.arange(clues_vals.shape[0])
-    col_inds = NVALS*NCOLS*clues_inds_rows + NVALS*clues_inds_cols + clues_vals-1
-    op_sparse = sp.sparse.csr_matrix( (data, (row_inds, col_inds)), shape = op_shape)
-    return op_sparse
+	data = np.ones((clues_vals.shape[0]), dtype=dtype)
+	row_inds = np.arange(clues_vals.shape[0])
+	col_inds = NVALS*NCOLS*clues_inds_rows + NVALS*clues_inds_cols + clues_vals-1
+	op_sparse = sp.sparse.csr_matrix( (data, (row_inds, col_inds)), shape = op_shape)
+	return op_sparse
 
 def generate_clues_contr_dense(clues_inds_rows, clues_inds_cols, clues_vals, dtype = int):
-    #x[i,j,m] = 1
-    op_shape = (clues_vals.shape[0], NROWS*NCOLS*NVALS)
-    op_dense = np.zeros(op_shape, dtype = dtype)
+	#x[i,j,m] = 1
+	op_shape = (clues_vals.shape[0], NROWS*NCOLS*NVALS)
+	op_dense = np.zeros(op_shape, dtype = dtype)
 
-    row_inds = np.arange(clues_vals.shape[0])
-    col_inds = NVALS*NCOLS*clues_inds_rows + NVALS*clues_inds_cols + clues_vals-1
-    op_dense[row_inds, col_inds] = 1
-    return op_dense
+	row_inds = np.arange(clues_vals.shape[0])
+	col_inds = NVALS*NCOLS*clues_inds_rows + NVALS*clues_inds_cols + clues_vals-1
+	op_dense[row_inds, col_inds] = 1
+	return op_dense
 
 @njit
 def generate_clues_contr_njit(clues_inds_rows, clues_inds_cols, clues_vals):
-    #x[i,j,m] = 1
-    op_shape = (clues_vals.shape[0], NROWS*NCOLS*NVALS)
-    op_dense = np.zeros(op_shape, dtype = numba.boolean)
-    
-    col_inds = NVALS*NCOLS*clues_inds_rows + NVALS*clues_inds_cols + clues_vals-1
-    for i, col_ind in enumerate(col_inds):
-        op_dense[i, col_ind] = True
-    return op_dense
+	#x[i,j,m] = 1
+	op_shape = (clues_vals.shape[0], NROWS*NCOLS*NVALS)
+	op_dense = np.zeros(op_shape, dtype = numba.boolean)
+	
+	col_inds = NVALS*NCOLS*clues_inds_rows + NVALS*clues_inds_cols + clues_vals-1
+	for i, col_ind in enumerate(col_inds):
+		op_dense[i, col_ind] = True
+	return op_dense
 
--- codes/python/utils/formattingUtils.py
+++ codes/python/utils/formattingUtils.py
@@ -1,32 +1,32 @@
 import numpy as np
 import scipy as sp
 from numba import njit
-
+	
 #you can also just turn the matrix into a sparse one in index format, that might be faster
 def sudoku_array_to_clues(clue_array):
-    clues_inds_rows, clues_inds_cols = np.where(clue_array>0)
-    clues_vals = clue_array[clues_inds_rows, clues_inds_cols]
-    return clues_inds_rows, clues_inds_cols, clues_vals
+	clues_inds_rows, clues_inds_cols = np.where(clue_array>0)
+	clues_vals = clue_array[clues_inds_rows, clues_inds_cols]
+	return clues_inds_rows, clues_inds_cols, clues_vals
 
 @njit
 def sudoku_array_to_clues_njit(clue_array):
-    clues_inds_rows, clues_inds_cols = np.where(clue_array>0)
-    clues_vals = np.empty_like(clues_inds_rows) 
-    for i, (row_ind, col_ind) in enumerate(zip(clues_inds_rows, clues_inds_cols)):
-        clues_vals[i] = clue_array[row_ind, col_ind]
-    return clues_inds_rows, clues_inds_cols, clues_vals
+	clues_inds_rows, clues_inds_cols = np.where(clue_array>0)
+	clues_vals = np.empty_like(clues_inds_rows) 
+	for i, (row_ind, col_ind) in enumerate(zip(clues_inds_rows, clues_inds_cols)):
+		clues_vals[i] = clue_array[row_ind, col_ind]
+	return clues_inds_rows, clues_inds_cols, clues_vals
 
 def array_to_vector(sudoku_array):
-    #turn a sudoku values array into a binary vector
-    flat_values = sudoku_array.flatten()
-    binary_values = np.zeros((flat_values.shape[0], 9), dtype = int)
-    indices = np.arange(flat_values.shape[0], dtype = int)
-    binary_values[indices, flat_values-1] = 1
-    return binary_values.flatten()
+	#turn a sudoku values array into a binary vector
+	flat_values = sudoku_array.flatten()
+	binary_values = np.zeros((flat_values.shape[0], 9), dtype = int)
+	indices = np.arange(flat_values.shape[0], dtype = int)
+	binary_values[indices, flat_values-1] = 1
+	return binary_values.flatten()
 
 def vector_to_array(binary_solution):
-    #turn a binary vector into a soduku values array
-    binary_values = binary_solution.reshape((81,9)) #magig numbers are sudoky dims and number of values possible
-    _, flat_values = np.where(binary_values)
-    sudoku_array = (flat_values+1).reshape(9,9)
-    return sudoku_array
+	#turn a binary vector into a soduku values array
+	binary_values = binary_solution.reshape((81,9)) #magig numbers are sudoky dims and number of values possible
+	_, flat_values = np.where(binary_values)
+	sudoku_array = (flat_values+1).reshape(9,9)
+	return sudoku_array
